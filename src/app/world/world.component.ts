43b84aaee1cce821f2fb90569b338665922190ae
ray-fisher <rachel.fisher3@outlook.com>
2023-11-25 14:35:54 -0600

    initial commit

---
+++ src/app/world/world.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-world',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './world.component.html',
+  styleUrl: './world.component.scss'
+})
+export class WorldComponent {
+
+}
