45c2f481ccf393ea46a2e4cb663bea169907fef3
ray-fisher <rachel.fisher3@outlook.com>
2023-11-25 15:32:33 -0600

    Routing to world component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WorldComponent } from "./world/world.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, WorldComponent]
 })
 export class AppComponent {
   title = 'D280';
