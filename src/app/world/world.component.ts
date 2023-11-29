8e24f72234c506636bd6ff8153eb3aa216840c2d
ray-fisher <rachel.fisher3@outlook.com>
2023-11-28 20:08:33 -0600

    Adding World Map, info table, and functions

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -9,5 +9,43 @@ import { CommonModule } from '@angular/common';
   styleUrl: './world.component.scss'
 })
 export class WorldComponent {
+  name: any;
+  capital: any;
+  region: any;
+  income: any;
+  latitude: any;
+  longitude: any;
 
+  ngOnInit(): void {
+    let countryInfo = document.querySelectorAll<SVGPathElement>('path');
+
+    Array.prototype.forEach.call(countryInfo, (country: SVGPathElement) => {
+
+      country.addEventListener('mouseover', (event:MouseEvent)=> {
+        const path = event.target as SVGPathElement;
+        path.style.fill = 'grey';
+        this.getCountyInfo(country);
+      });
+
+      country.addEventListener('mouseleave', (event:MouseEvent)=> {
+        const path = event.target as SVGPathElement;
+        path.style.fill = '';
+      });
+    });
+  }
+
+  async getCountyInfo(country: SVGPathElement){
+    let api: string = 'https://api.worldbank.org/V2/country/'+country.id+'?format=json';
+    let response: Response = await fetch(api);
+    let data: any =  await response.json();
+    let dataPath: any = data[1];
+    
+    this.name = dataPath[0].name;
+    this.capital = dataPath[0].capitalCity;
+    this.region = dataPath[0].region.value;
+    this.income = dataPath[0].incomeLevel.value;
+    this.longitude = dataPath[0].longitude;
+    this.latitude = dataPath[0].latitude;
+
+  }
 }
